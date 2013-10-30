770b851782d4b1fe06a5ccd99ca5637b8bf2d5ec
WonderBeat <borov.htid@gmail.com>
2013-10-30 16:49:19 +0400

    Exec still not working on CI. Need more logs

--- src/main/kotlin/com/vasilich/commands/basic/ExecCommand.kt
+++ src/main/kotlin/com/vasilich/commands/basic/ExecCommand.kt
@@ -10,6 +10,7 @@ import java.io.ByteArrayOutputStream
 import org.apache.commons.exec.ExecuteWatchdog
 import java.text.MessageFormat
 import org.springframework.core.io.FileSystemResource
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 
 Component
@@ -22,6 +23,8 @@ public class ExecUnit(val aliases: Array<String> = array(), val script: String =
 
 Component
 public class ExecCommand [Autowired] (private val cfg: ExecCgf): Command {
+
+    val logger = LoggerFactory.getLogger(this.javaClass)!!;
 
     val osname = System.getProperty("os.name")!!.toLowerCase()
 
@@ -48,7 +51,9 @@ public class ExecCommand [Autowired] (private val cfg: ExecCgf): Command {
         val stdout = ByteArrayOutputStream()
         val psh = PumpStreamHandler(stdout)
         val scriptResource = FileSystemResource(pickProperScript(script))
-        val cl = CommandLine.parse(scriptPrefix + scriptResource.getFile()!!.getAbsolutePath())
+        val cmd = scriptPrefix + scriptResource.getFile()!!.getAbsolutePath()
+        logger.debug("Exec: ${cmd}")
+        val cl = CommandLine.parse(cmd)
         val exec = DefaultExecutor()
         exec.setWatchdog(ExecuteWatchdog(cfg.timeout))
         exec.setStreamHandler(psh)
