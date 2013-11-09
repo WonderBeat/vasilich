0a831899387c4cab43cf16a13fead401fda55124
Alexey Udalov <al.udalec@gmail.com>
2013-11-09 17:10:47 +0400

    Added Basic Talk module

--- src/test/kotlin/functional/ChatTest.kt
+++ src/test/kotlin/functional/ChatTest.kt
@@ -49,5 +49,9 @@ public class ChatTest() {
                 { it?.contains("Invisible exception occurs")!! },
                 "Vasilich should launch script and prints output during script execution")
         replyFor("Vasilich, WTF?", { it != null }, "Vasilich is talkative. He should response ;)")
+        replyFor("Vasilich, Hi", { it?.contains("Hello")!! }, "Vasilich is talkative. He should response.")
+        replyFor("Vasilich, Good morning", { it?.contains("Hello")!! }, "Vasilich is talkative. He should response.")
+        replyFor("Vasilich, Chao", { it?.contains("Bye, see you")!! }, "Vasilich is talkative. He should response.")
+        replyFor("Vasilich, Do you know any good IT place to work in Spb?", { it?.contains("EPAM")!! }, "Vasilich is talkative. He should response.")
     }
 }
