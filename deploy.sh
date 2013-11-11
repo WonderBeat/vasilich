3046f332fa7edc4e40fc1d7e90e0bd2d5acf79a7
WonderBeat <borov.htid@gmail.com>
2013-11-11 19:02:42 +0400

    Travis. Replace spaces with tabs. Should help in ssh conf

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,6 @@ if [ -n "$PASSWORD" ]; then
     openssl aes-256-cbc -k $PASSWORD -in deploy_key.enc -d -a -out id_rsa
     chmod 600 id_rsa
     ID_FILE=$(pwd)/id_rsa
-    echo "Host github.com" >> ~/.ssh/config
-    echo " StrictHostKeyChecking no" >> ~/.ssh/config
-    echo " CheckHostIP no" >> ~/.ssh/config
-    echo " IdentityFile $ID_FILE" >> ~/.ssh/config
     cd mvn-repo
     git config --global user.email "WonderBeat@github.com"
     git config --global user.name "WonderBeat"
