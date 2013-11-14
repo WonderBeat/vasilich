af754430260031a5c702a9ead4636ca94af051a1
Ilya Mestnikov <ilya.mestnikov@gmail.com>
2013-11-15 01:16:50 +0400

    Disk and Cpu entities have been introduced. Corresponding commands have been rewritten

--- src/main/kotlin/com/vasilich/commands/monitoring/CpuUsageCommand.kt
+++ src/main/kotlin/com/vasilich/commands/monitoring/CpuUsageCommand.kt
@@ -2,40 +2,22 @@ package com.vasilich.commands.monitoring
 
 import org.springframework.stereotype.Component
 import com.vasilich.commands.api.Command
-import javax.management.MBeanServerConnection
-import java.lang.management.OperatingSystemMXBean
 import java.lang.management.ManagementFactory
+import com.sun.management.OperatingSystemMXBean
+import com.vasilich.system.Cpu
 
 /**
  *
  * @author Ilya_Mestnikov
- * @date 11/13/13
- * @time 3:02 AM
+ *
  */
 Component
 public class CpuUsageCommand: Command {
+
     override fun execute(msg: String): String? {
-        val mbsc = ManagementFactory.getPlatformMBeanServer()!!;
 
-        val osMBean = ManagementFactory.newPlatformMXBeanProxy(
-            mbsc, ManagementFactory.OPERATING_SYSTEM_MXBEAN_NAME, javaClass<OperatingSystemMXBean>());
-
-        val nanoBefore = System.nanoTime();
-        /*val cpuBefore = osMBean.getProcessCpuTime();
-
-        // Call an expensive task, or sleep if you are monitoring a remote process
-
-        long cpuAfter = osMBean.getProcessCpuTime();
-        long nanoAfter = System.nanoTime();
-
-        long percent;
-        if (nanoAfter > nanoBefore)
-            percent = ((cpuAfter-cpuBefore)*100L)/
-            (nanoAfter-nanoBefore);
-        else percent = 0;
-
-        System.out.println("Cpu usage: "+percent+"%");*/
-        return ""
+        val percent = Cpu().getSystemLoad() * 100;
+        return "Current cpu usage: %.2f%%".format(percent);
     }
 
 }
--- src/main/kotlin/com/vasilich/commands/monitoring/DiskUsageCommand.kt
+++ src/main/kotlin/com/vasilich/commands/monitoring/DiskUsageCommand.kt
@@ -2,12 +2,8 @@ package com.vasilich.commands.monitoring
 
 import org.springframework.stereotype.Component
 import com.vasilich.commands.api.Command
-import java.nio.file.FileSystem
-import java.nio.file.FileStore
-import java.nio.file.FileSystems
-import java.nio.file.Paths
-import java.io.PrintStream
-import org.apache.commons.lang3.SystemUtils
+import com.vasilich.system.FileSystem
+import com.vasilich.system.Size
 
 /**
  *
@@ -18,26 +14,16 @@ import org.apache.commons.lang3.SystemUtils
 Component
 public class DiskUsageCommand : Command {
 
-    private val K = 1024*1000*1000
-
     override fun execute(msg: String): String? {
 
         var output = "Disk Usage Statistics:"
-        val fs = FileSystems.getDefault();
 
-        for (store in fs?.getFileStores()!!) {
-            val total = store.getTotalSpace() / K;
-            val used = (store.getTotalSpace() - store.getUnallocatedSpace()) / K;
-            val avail = store.getUsableSpace() / K;
+        val fs = FileSystem()
 
-            var diskName = store.toString()!!
+        for (disk in fs.disks) {
+            output += "\n%-4s T:%5dGb U:%4dGb A:%4dGb".format(disk.name,
+                    Size.Gb.get(disk.total), Size.Gb.get(disk.used), Size.Gb.get(disk.available));
 
-            if (SystemUtils.IS_OS_WINDOWS) {
-                diskName = diskName.split("\\s").filter {
-                    it.matches("^\\([A-Z]:\\)$")
-                }.first as String
-            }
-            output += "\n%-4s T:%5dGb U:%4dGb A:%4dGb".format(diskName, total, used, avail);
         }
         return output
     }
