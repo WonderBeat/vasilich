d3919c5c8ee7620620d2fd2452c8e900a3f20753
WonderBeat <borov.htid@gmail.com>
2013-11-22 15:20:03 +0400

    Version removed from shaded JAR file. It simplifies deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ -n "$PASSWORD" ]; then
     # decrypt -> openssl aes-256-cbc -k "pwd" -in deploy_key.enc -d -a -out id_rsa
     openssl aes-256-cbc -k $PASSWORD -in dropbox_app_key.enc -d -a -out dropbox_app_key.dec
     chmod +x dropbox_uploader.sh
-    ./dropbox_uploader.sh -f dropbox_app_key.dec upload target/vasilich-1.0-SNAPSHOT.jar Public/vasilich/vasilich-alpha.jar
+    ./dropbox_uploader.sh -f dropbox_app_key.dec upload target/vasilich.jar Public/vasilich/vasilich-alpha.jar
 else
     echo "Can't deploy without a password"
 fi
